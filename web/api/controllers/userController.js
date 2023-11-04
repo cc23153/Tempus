d5e6099b25371936823f072740f1d01f782551cd
zBl4ck <zBl4ckDev@protonmail.com>
2023-11-04 18:57:14 -0300

    Add deleteUser + patch methods

--- web/api/controllers/userController.js
+++ web/api/controllers/userController.js
@@ -1,16 +1,33 @@
 const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 
-exports.getUser = (('/'), async (req, res) => {
+const userExistsById = async (user_id) => {
     const response = await prisma.user.findUnique({
         where: {
-            username: req.body.username,
+            user_id: user_id
         },
     })
+    return response;
+}
+
+const userExistsByUsername = async (username) => {
+    const response = await prisma.user.findUnique({
+        where: {
+            username: username
+        },
+    })
+    return response
+}
+
+
+exports.getUser = (('/'), async (req, res) => {
+    const response =  await userExistsByUsername(req.body.username)
     if(response)
         res.status(200).json(response)
-    else
-        res.status(401).json({error: 'true', message: 'User doesn\'t exist'})
+    else 
+        res.status(400).json({
+            error: 'true', message: 'User doesn\'t exist'
+        })
 })
 
 exports.postUser = (('/'), async (req, res) => {
@@ -38,6 +55,72 @@ exports.postUser = (('/'), async (req, res) => {
 })
 
 exports.putUser = (('/'), async (req, res) => {
+
+})
+
+exports.deleteUser = ( ('/'), async (req, res) => {
+    const user_id = req.body.user_id
+    const response = await userExistsById(user_id)
+
+    if(!response){
+        res.status(400).json({error: 'true', message: 'User doesn\'t exist'})
+    }else{
+        await prisma.$queryRaw`exec Tempus.spDeleteUser ${user_id}` 
+        res.status(200).json({error: 'false', message: 'User deleted'})
+    }
+
+})
+
+exports.patchUserUsername = ( ('/'), async (req, res) => {
+    const user_id = req.body.user_id
+    const username = req.body.username
+    const response = await userExistsById(user_id)
+    if(!response){
+        res.status(400).json({error: 'true', message: 'User doesn\'t exist'})
+    }else{
+        await prisma.$queryRaw`exec Tempus.spUpdateUsername ${user_id}, ${username}` 
+        res.status(200).json({error: 'false', message: 'Username succesfully updated'})
+    }
+
+})
+
+exports.patchUserNickname = ( ('/'), async (req, res) => {
+    const user_id = req.body.user_id
+    const nickname = req.body.nickname
+    const response = await userExistsById(user_id)
+    if(!response){
+        res.status(400).json({error: 'true', message: 'User doesn\'t exist'})
+    }else{
+        await prisma.$queryRaw`exec Tempus.spUpdateUserNickname ${user_id}, ${nickname}` 
+        res.status(200).json({error: 'false', message: 'Nickname succesfully updated'})
+    }
+
+})
+
+exports.patchUserEmail = ( ('/'), async (req, res) => {
+    const user_id = req.body.user_id
+    const email = req.body.email
+    const response = await userExistsById(user_id)
+    if(!response){
+        res.status(400).json({error: 'true', message: 'User doesn\'t exist'})
+    }else{
+        await prisma.$queryRaw`exec Tempus.spUpdateUserEmail ${user_id}, ${email}` 
+        res.status(200).json({error: 'false', message: 'Email succesfully updated'})
+    }
+})
+
+exports.putUser = ( ('/'), async (req, res) => {
+    const user_id = req.body.user_id
+    const username = req.body.username
+    const nickname = req.body.nickname
+    const email = req.body.email
+    const response = await userExistsById(user_id)
+    if(!response){
+        res.status(400).json({error: 'true', message: 'User doesn\'t exist'})
+    }else{
+        await prisma.$queryRaw`exec Tempus.spUpdateUser ${user_id}, ${username}, ${nickname}, ${email}` 
+        res.status(200).json({error: 'false', message: 'User information succesfully updated'})
+    }
 
 })
 
--- web/api/routes/userRoutes.js
+++ web/api/routes/userRoutes.js
@@ -4,5 +4,9 @@ const userController = require('../controllers/userController')
 router.get('/',userController.getUser)
 router.post('/', userController.postUser)
 router.put('/', userController.putUser)
+router.delete('/', userController.deleteUser)
+router.patch('/patchemail', userController.patchUserEmail)
+router.patch('/patchnickname', userController.patchUserNickname)
+router.patch('/patchusername', userController.patchUserUsername)
 
 module.exports = router
