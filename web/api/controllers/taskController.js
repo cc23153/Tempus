ad41a1ea3ff4e5060de62f0902bd15e35a28fa59
zBl4ck <zBl4ckDev@protonmail.com>
2023-11-04 23:20:32 -0300

    Task delete, patch and get

--- web/api/controllers/taskController.js
+++ web/api/controllers/taskController.js
@@ -1,12 +1,73 @@
 const {PrismaClient} = require('@prisma/client')
 const prisma = new PrismaClient()
 
+const taskExist =  async (task_id) => {
+    const task = await prisma.task.findUnique({
+        where: {
+            task_id: task_id
+        }
+    })
+    return task
+}
+
 exports.getTask = (('/'),async(req, res)=>{
-    await res.json('msg: "In construction"')
+    const task_id = req.body.task_id
+    const task = await taskExist(task_id)
+    if(!task){
+        res.status(400).json({error: true, message : "Task doesn't exist"})
+        return
+    }
+    res.status(200).json(task)
 })
 
 exports.deleteTask = (('/'), async(req, res)=> {
-    await res.json('msg: "In construction"') 
+    const task_id = req.body.task_id
+    const task = await taskExist(task_id)
+    if(!task){
+        res.status(400).json({error: true, message : "Task doesn't exist"})
+        return
+   }
+   
+   await prisma.$queryRaw`exec Tempus.spDeleteTask ${task_id}`
+   res.status(200).json({error: false, message: "Task succesfully deleted"})
+})
+
+exports.patchTaskName = (('/'), async(req, res)=> {
+    const task_id = req.body.task_id
+    const task_name = req.body.task_name
+    const task = await taskExist(task_id)
+    if(!task){
+        res.status(400).json({error: true, message : "Task doesn't exist"})
+        return
+   }
+   console.log(task_name)
+   await prisma.$queryRaw`exec Tempus.spUpdateTaskName ${task_id}, ${task_name}`
+   res.status(200).json({error: false, message: "Task succesfully updated"})
+})
+
+exports.patchTaskDescription = (('/'), async(req, res)=> {
+    const task_id = req.body.task_id
+    const task_description = req.body.task_description
+    const task = await taskExist(task_id)
+    if(!task){
+        res.status(400).json({error: true, message : "Task doesn't exist"})
+        return
+   }
+   
+   await prisma.$queryRaw`exec Tempus.spUpdateTaskDescription ${task_id}, ${task_description}`
+   res.status(200).json({error: false, message: "Task succesfully updated"})
+})
+exports.patchTaskSituation = (('/'), async(req, res)=> {
+    const task_id = req.body.task_id
+    const task_situation = req.body.task_situation
+    const task = await taskExist(task_id)
+    if(!task){
+        res.status(400).json({error: true, message : "Task doesn't exist"})
+        return
+   }
+   
+   await prisma.$queryRaw`exec Tempus.spUpdateTaskSituation ${task_id}, ${task_situation}`
+   res.status(200).json({error: false, message: "Task succesfully updated"})
 })
 
 
--- web/api/routes/taskRoutes.js
+++ web/api/routes/taskRoutes.js
@@ -2,5 +2,9 @@ const router  = require('express').Router()
 const taskController = require('../controllers/taskController')
 
 router.get('/',taskController.getTask)
+router.delete('/',taskController.deleteTask)
+router.patch('/patchdescription', taskController.patchTaskDescription)
+router.patch('/patchname', taskController.patchTaskName)
+router.patch('/patchsituation', taskController.patchTaskSituation)
 
 module.exports = router
