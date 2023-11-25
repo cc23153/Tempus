e27b302dcef4ffc30a7c438e9c9e64ea5408c7b1
zBl4ck <zBl4ckDev@protonmail.com>
2023-11-25 11:29:52 -0300

    use .env to configure api and frontend ports

--- web/api/app.js
+++ web/api/app.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const router = require('./routes/router')
+const cookieParser = require('cookie-parser')
 const cors = require('cors')
 const app = express()
 
-const PORT = 5050
+const PORT = process.env.API_PORT
+const FRONTEND_PORT = process.env.FRONTEND_PORT
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
 
 const corsOptions = {
-  origin: 'http://localhost:5000',
+  origin: `http://localhost:${FRONTEND_PORT}`,
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 };
 
