3da99a986729ddeb869f3f92fb88cac31ebf74ab
zBl4ck <zBl4ckDev@protonmail.com>
2023-11-29 20:40:08 -0300

    feat: add teammembers and category to main router

--- web/api/routes/router.js
+++ web/api/routes/router.js
@@ -1,17 +1,22 @@
 const router = require('express').Router()
 
 const userRoutes = require('./userRoutes')
-const taskRoutes = require('./taskRoutes')
 const workspaceRoutes = require('./workspaceRoutes')
 const authRoutes = require('./authRoutes')
 const commentRoutes = require('./commentRoutes')
+const categoryRoutes = require('./categoryRoutes')
+const taskRoutes = require('./taskRoutes')
 const teamRoutes = require('./teamRoutes')
+const teammembersRoutes = require('./teammembersRoutes')
+
 
 router.use('/u', userRoutes)
-router.use('/ta', taskRoutes)
 router.use('/w', workspaceRoutes)
 router.use('/a', authRoutes)
-router.use('/c', commentRoutes)
+router.use('/co', commentRoutes)
+router.use('/ca', categoryRoutes)
+router.use('/ta', taskRoutes)
 router.use('/te', teamRoutes)
+router.use('/tm', teammembersRoutes)
 
 module.exports = router
