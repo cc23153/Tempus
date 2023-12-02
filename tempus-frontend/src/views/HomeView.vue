f79073ec1865578287b7f948eb4b1c28bca4dec0
zBl4ck <zBl4ckDev@protonmail.com>
2023-12-02 01:44:59 -0300

    cleaning the project

--- tempus-frontend/src/views/HomeView.vue
+++ tempus-frontend/src/views/HomeView.vue
@@ -1,10 +1,8 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <!-- <TheWelcome /> -->
     <h1>Hello, World!</h1>
     <h2>Hello world from Tempus!!</h2>
   </main>
