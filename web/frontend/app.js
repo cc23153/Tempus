fb0768f59ce599a08618ec91b8f6d9a649dbae11
Ana <cc23122@g.unicamp.br>
2023-12-02 12:06:39 -0300

    atualização das páginas

--- web/frontend/app.js
+++ web/frontend/app.js
@@ -1,5 +1,5 @@
-require('dotenv').config();
-const secretKey = process.env.SECRET_KEY;
+// require('dotenv').config();
+// const secretKey = process.env.SECRET_KEY;
 
 const express = require('express')
 const path = require('path')
