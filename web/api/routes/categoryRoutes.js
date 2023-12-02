257fbb5e4e8b7700203fd034fbd1e0e38540f096
Ana <cc23122@g.unicamp.br>
2023-12-02 18:34:18 -0300

    Select com as categorias

--- web/api/routes/categoryRoutes.js
+++ web/api/routes/categoryRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const categoryController = require('../controllers/categoryController')
 
-router.get(('/'), categoryController.getCategory)
+router.get(('/get'), categoryController.getCategory)
 router.post(('/'), categoryController.postCategory)
 router.put(('/'), categoryController.putCategory)
 router.delete(('/'), categoryController.deleteCategory)
