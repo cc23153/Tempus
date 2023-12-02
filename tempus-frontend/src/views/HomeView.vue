9ffca6450e38f57cc2dace90a93d770db5462a12
zBl4ck <zBl4ckDev@protonmail.com>
2023-12-02 00:21:11 -0300

    feat(vue): :sparkles: Begin the work with Vue

---
+++ tempus-frontend/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <!-- <TheWelcome /> -->
+    <h1>Hello, World!</h1>
+    <h2>Hello world from Tempus!!</h2>
+  </main>
+</template>
