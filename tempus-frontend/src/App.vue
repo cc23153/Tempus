9a4073974e0404d12fc8f28477806d952666f5ab
zBl4ck <zBl4ckDev@protonmail.com>
2023-12-03 15:42:32 -0300

    remove Header from App.vue

--- tempus-frontend/src/App.vue
+++ tempus-frontend/src/App.vue
@@ -1,35 +1,14 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HeaderComponent from './components/HeaderComponent.vue';
 </script>
 
 <template>
-  <HeaderComponent v-if="showHeader"/>
   <RouterView/>
 </template>
 
 <script>
-import Header from './components/HeaderComponent.vue'; // Importe o componente da barra de navegação
+import Header from './components/Header.vue';
 
-export default {
-  components: {
-    Header
-  },
-  data() {
-    return {
-      showHeader: true
-    };
-  },
-  beforeRouteEnter(to, from, next) {
-    if (to.path === '/login') {
-      next(vm => {
-        vm.showHeader = false;
-      });
-    } else {
-      next();
-    }
-  }
-};
 </script>
 
 
