b938a61f3fb2e030156513803cd1c432655a99ee
zBl4ck <zBl4ckDev@protonmail.com>
2023-12-03 22:17:22 -0300

    basic 404 page

--- tempus-frontend/src/router/index.js
+++ tempus-frontend/src/router/index.js
@@ -1,6 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import Login from '../views/LoginView.vue'
+import NotFound from '../views/NotFound.vue'
+import ResetPassword from '../views/ResetPasswordView.vue'
+import Signup from '../views/SignupView.vue'
+import UserHome from '../views/UserHomeView.vue'
+
+const token = document.cookie
+        .split("; ")
+        .find((row) => row.startsWith("token="))
+        ?.split("=")[1];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,7 +17,15 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: () => {
+        return new Promise((resolve) => {
+          if (token) {
+            resolve(UserHome);
+          } else {
+            resolve(HomeView);
+          }
+        });
+      }
     },
     {
       path: '/login',
@@ -21,12 +38,24 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/SignupView.vue')
+      component: () => Signup
     },
     {
       path: '/resetpassword',
       name: 'resetpassword',
-      component: () => import ('../views/ResetPasswordView.vue')
+      component: ResetPassword
+    },
+    {
+      path: '/u/:username',
+      name: 'user-profile',
+      component: UserHome,  
+
+      
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'not-found',
+      component: NotFound
     }
   ]
 })
