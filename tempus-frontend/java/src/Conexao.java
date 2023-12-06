90704aa433f5689bd71ab99a1a86e3bc7e4ced7a
zBl4ckUser <zBl4ckUser@protonmail.com>
2023-12-06 12:37:03 -0300

    commit++

--- tempus-frontend/java/src/Conexao.java
+++ tempus-frontend/java/src/Conexao.java
@@ -3,6 +3,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Conexao {
 
@@ -11,31 +12,41 @@ public class Conexao {
 
         String usuario = "BD23153";
         String senha = "BD23153";
-        int user_id = 1;
+        //String username = "zbl4ck";
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite o seu username: ");
+        String username = input.next();
+
+        input.close();
 
         try {
             Connection conexao = DriverManager.getConnection("jdbc:sqlserver://regulus.cotuca.unicamp.br:1433;database=BD23153;user=BD23153;password=BD23153;encrypt=false;trustServerCertificate=true");
             System.out.println("Entrou no banco de dados.");
 
-            String queryUsername = "SELECT username FROM Tempus.[User] WHERE user_id = ?";
-            String queryNickname = "SELECT nickname FROM Tempus.[User] WHERE user_id = ?";
-            String queryEmail = "SELECT email FROM Tempus.[User] WHERE user_id = ?";
-            String queryNumTasks = "SELECT COUNT(task_id) FROM Tempus.Task WHERE user_id = ?";
+            String queryUser_id = "SELECT user_id FROM Tempus.[User] WHERE username = ?";
+            String queryNickname = "SELECT nickname FROM Tempus.[User] WHERE username = ?";
+            String queryEmail = "SELECT email FROM Tempus.[User] WHERE username = ?";
+            // String queryNumTasks = "SELECT register_datetime FROM Tempus.[User] WHERE user_id = ?";
 
-            try (PreparedStatement pstmtUsername = conexao.prepareStatement(queryUsername);
+             try (PreparedStatement pstmtUser_id = conexao.prepareStatement(queryUser_id);
                  PreparedStatement pstmtNickname = conexao.prepareStatement(queryNickname);
                  PreparedStatement pstmtEmail = conexao.prepareStatement(queryEmail);
-                 PreparedStatement pstmtNumTasks = conexao.prepareStatement(queryNumTasks)) {
+                //  PreparedStatement pstmtNumTasks = conexao.prepareStatement(queryNumTasks);
+                ) {
 
-                pstmtUsername.setInt(1, user_id);
-                pstmtNickname.setInt(1, user_id);
-                pstmtEmail.setInt(1, user_id);
-                pstmtNumTasks.setInt(1, user_id);
+                pstmtUser_id.setString(1, username);
+                pstmtNickname.setString(1, username);
+                pstmtEmail.setString(1, username);
 
-                String username = executeQueryAndGetString(pstmtUsername);
+                int user_id = executeQueryAndGetInt(pstmtUser_id);
+                // pstmtNumTasks.setInt(1, user_id);
+
+                
                 String nickname = executeQueryAndGetString(pstmtNickname);
                 String email = executeQueryAndGetString(pstmtEmail);
-                int numTasks = executeQueryAndGetInt(pstmtNumTasks);
+                // int numTasks = executeQueryAndGetInt(pstmtNumTasks);
+                int numTasks = 70;
 
                 if (numTasks <= 0) {
                     throw new IllegalArgumentException("O usuário não possui nenhuma task.");
@@ -43,9 +54,11 @@ public class Conexao {
 
                 if (numTasks < 50) {
                     User user = new User(username, nickname, email, numTasks);
+                    System.out.println(user.toString());
                     // Further processing for user
                 } else {
                     UserWithStatus userWithStatus = new UserWithStatus(username, nickname, email, numTasks);
+                    System.out.println(userWithStatus.toString());
                     // Further processing for userWithStatus
                 }
             }
