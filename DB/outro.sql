bac2de7fb93327313f28b0e8979721424ded41f7
Luiz Inácio Lula da Silva <142425086+cc23142@users.noreply.github.com>
2023-12-06 10:02:43 -0300

    tregger e view

--- DB/outro.sql
+++ DB/outro.sql
@@ -28,9 +28,51 @@ as
     select T.team_id, T.team_name, U.user_id, U.username from [Tempus].[Team] T,
         ([Tempus].[TeamMembers] TM join [Tempus].[User] U on TM.user_id = U.user_id) where TM.team_id = T.team_id 
 
-
-/* -=-=-=-=-=-=-=-=-=-= TRIGGERS -=-=-=-=-=-=-=-=-=- */
 go 
+/* pega uma lista de tarefas com seus IDs, nomes e a contagem de comentarios */
+CREATE OR ALTER VIEW [Tempus].[CommentsPerTask]
+AS
+    SELECT T.task_id, T.task_name, COUNT(C.comment_id) AS num_comments
+    FROM [Tempus].[Task] T
+    LEFT JOIN [Tempus].[Comment] C ON T.task_id = C.task_id
+    GROUP BY T.task_id, T.task_name;
+/*Pega os nomes e usernames dos times*/
+CREATE OR ALTER VIEW [Tempus].[UsersByTeam]
+AS
+SELECT T.team_name, U.username, U.nickname
+FROM [Tempus].[Team] T
+INNER JOIN [Tempus].[TeamMembers] TM ON T.team_id = TM.team_id
+INNER JOIN [Tempus].[User] U ON TM.user_id = U.user_id;
+
+/* pega as categorias e as tasks concluidas de cada categoria*/
+CREATE OR ALTER VIEW [Tempus].[CompletedTasksByCategory]
+AS
+SELECT C.category_name, COUNT(T.task_id) AS completed_tasks
+FROM [Tempus].[Task] T
+INNER JOIN [Tempus].[Category] C ON T.task_category = C.category_id
+WHERE T.task_situation = 'Concluída'
+GROUP BY C.category_name;
+
+/* pega o nome task, o nome da categoria e o nome do time selecionado*/
+CREATE OR ALTER VIEW [Tempus].[TasksByUserAndTeam]
+AS 
+SELECT U.username, T.task_name, Te.team_name
+FROM [Tempus].[Task] T
+INNER JOIN [Tempus].[Workspace] W ON T.workspace_id = W.workspace_id
+INNER JOIN [Tempus].[Team] Te ON W.team_id = Te.team_id
+INNER JOIN [Tempus].[TeamMembers] TM ON Te.team_id = TM.team_id
+INNER JOIN [Tempus].[User] U ON TM.user_id = U.user_id;
+
+/* o nome da categoria, pega o nome task e o nome do time*/
+CREATE OR ALTER VIEW [Tempus].[TasksByCategoryAndTeam]
+AS 
+SELECT C.category_name, T.task_name, Te.team_name
+FROM [Tempus].[Task] T
+INNER JOIN [Tempus].[Category] C ON T.task_category = C.category_id
+INNER JOIN [Tempus].[Workspace] W ON T.workspace_id = W.workspace_id
+INNER JOIN [Tempus].[Team] Te ON W.team_id = Te.team_id;
+/* -=-=-=-=-=-=-=-=-=-= TRIGGERS -=-=-=-=-=-=-=-=-=- */
+
 
 -- Trigger para TeamMembers
 CREATE TRIGGER trDeleteTeamMembers
@@ -121,3 +163,32 @@ BEGIN
 END;
 
 go
+
+--Tregger que Previne a deletação de uma categoria com task
+CREATE TRIGGER trPreventCategoryDeletion
+ON [Tempus].[Category]
+INSTEAD OF DELETE
+AS
+BEGIN
+    IF EXISTS (SELECT 1 FROM [Tempus].[Task] WHERE task_category IN (SELECT deleted.category_id FROM deleted))
+    BEGIN
+        RAISERROR('Não é possível excluir categorias com tarefas associadas!', 16, 1);
+    END
+    ELSE
+    BEGIN
+        DELETE FROM [Tempus].[Category] WHERE category_id IN (SELECT category_id FROM deleted);
+    END
+END;
+
+--Mostra a situação da tesk apos a alteração
+CREATE TRIGGER trUpdateTaskSituation
+ON [Tempus].[Task]
+AFTER UPDATE
+AS
+BEGIN
+    -- Atualiza a situação da tarefa automaticamente quando for alterada
+    UPDATE T
+    SET T.task_situation = I.task_situation
+    FROM [Tempus].[Task] T
+    INNER JOIN inserted I ON T.task_id = I.task_id;
+END;
